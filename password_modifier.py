fcd6c6fba2b0f71bfc5655d5e9c39d87d4e32a35
ashtonchiang <77808315+ashtonchiang@users.noreply.github.com>
2021-03-02 22:09:27 -0600

    Add files via upload

---
+++ password_modifier.py
@@ -0,0 +1,23 @@
+
+passwrd=input()
+password=''
+for x in passwrd:
+    if(x=='i'):
+        password+="!"          
+    elif(x=='a'):
+        password+="@"
+    elif(x=='m'):
+        password+="M"
+    elif(x=='B'):
+        password+="8"
+    elif(x=='o'):
+        password+="."
+    else:
+        password+= x
+
+password+="q*s"
+
+print(password)
+
+
+
